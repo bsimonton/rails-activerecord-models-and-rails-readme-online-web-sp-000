784efb6f8a37f5fbf7f37d32e4ccb6b4979da039
Brian Simonton <bsimonton78@gmail.com>
2019-08-06 23:15:53 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,7 +8,10 @@ describe Post do
   end
  
  
- 
+ it 'has a summary' do
+  post = Post.create!(title: "My title", description: "The post description")
+  expect(post.post_summary).to eq("My title - The post description")
+end
  
  
  
